4ac44dbb639b1e8dc4ebbfc9dcab5f4cd389fc22
3str-webdev <119620887+3str-webdev@users.noreply.github.com>
2023-05-12 17:15:04 +0300

    feat: add default styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 
+import './styles/app.scss';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<ChakraProvider>
