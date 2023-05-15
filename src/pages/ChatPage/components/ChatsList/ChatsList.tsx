602bc19981be56422ef237949c8b2aca33f4b86f
3str-webdev <119620887+3str-webdev@users.noreply.github.com>
2023-05-15 12:34:30 +0300

    feat: apply AddChatButton

--- src/pages/ChatPage/components/ChatsList/ChatsList.tsx
+++ src/pages/ChatPage/components/ChatsList/ChatsList.tsx
@@ -1,21 +1,33 @@
 import { Stack } from '@chakra-ui/layout';
+import { Divider, Flex } from '@chakra-ui/react';
 import { useAppSelector } from '@redux/hooks';
 import { FC } from 'react';
+import AddChat from '../AddChat/AddChat';
 import ChatItem from '../ChatItem/ChatItem';
 
 const ChatsList: FC = () => {
 	const chats = useAppSelector(state => state.chatsReducer);
 	return (
-		<Stack overflowY={'auto'}>
-			{chats.map(chat => {
-				return (
-					<ChatItem
-						key={chat.chatId}
-						chat={chat}
-					/>
-				);
-			})}
-		</Stack>
+		<Flex
+			direction={'column'}
+			gap={2}
+		>
+			<AddChat />
+			<Divider />
+			<Stack
+				flex={1}
+				overflowY={'auto'}
+			>
+				{chats.map(chat => {
+					return (
+						<ChatItem
+							key={chat.chatId}
+							chat={chat}
+						/>
+					);
+				})}
+			</Stack>
+		</Flex>
 	);
 };
 
