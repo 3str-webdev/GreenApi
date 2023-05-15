50e0fbdca6a6e21188d31ff9244274dbd6911da2
3str-webdev <119620887+3str-webdev@users.noreply.github.com>
2023-05-15 14:02:44 +0300

    fix: update imports

--- src/redux/hooks/useAppDispatch.ts
+++ src/redux/hooks/useAppDispatch.ts
@@ -1,4 +1,4 @@
-import { AppDispatch } from '@store/store';
+import { AppDispatch } from '@redux/store/store';
 import { useDispatch } from 'react-redux';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
